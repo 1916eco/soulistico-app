8b85b936218fb5c7de75300ecd56218ac86f6338
1916eco <1916eco@gmail.com>
2022-05-11 20:04:23 +0100

    navbar intigration

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+//Home component
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Home!</Text>
+      </View>
+      )
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Not Home" component={HomeScreen} />
+        <Tab.Screen name="NotHome" component={HomeScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
